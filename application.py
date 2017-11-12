20359d1343e51426f3959a4744c0a8e4d70f1bd1
cohenta2 <cohenta2@vcu.edu>
2017-11-12 01:03:38 -0500

    adding map page

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from urllib import request
+import collections
+import numpy as np
 import json
 
 response = request.urlopen("https://api.reddit.com/subreddits/popular/?limit=100")
@@ -20,3 +22,13 @@ def index():
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+@app.route('/map')
+def build_map():
+    columns = np.arange(10)
+    result = collections.defaultdict(list)
+    for d in top100_subreddits:
+        result[d['name']].append(d)
+
+    result_list = result.values()
+    return render_template('map.html', columns = columns, subreddits = result_list, random="test")
